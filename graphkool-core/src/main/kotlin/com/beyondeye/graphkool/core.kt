8b878f00e2262e2dfc36a3ae608c9403d89db4cf
daely <dario-e@eye-beyond.com>
2016-12-08 18:14:05 +0200

    initial rewrite of StarWarsSchema, missing special syntax for argument

--- graphkool-core/src/main/kotlin/com/beyondeye/graphkool/core.kt
+++ graphkool-core/src/main/kotlin/com/beyondeye/graphkool/core.kt
@@ -11,10 +11,22 @@ fun newGraphQL(schemaBuilder: GraphQLSchema.Builder, executionStrategy: Executio
 fun newGraphQL(schema: GraphQLSchema, executionStrategy: ExecutionStrategy?=null) = GraphQL(schema,executionStrategy)
 fun newObject(name:String)= GraphQLObjectType.newObject().name(name)
 fun newEnum(name:String)= GraphQLEnumType.newEnum().name(name)
+fun newInterface(name:String)=GraphQLInterfaceType.newInterface().name(name)
 infix fun String.ofType(type_: GraphQLOutputType): GraphQLFieldDefinition.Builder {
     return GraphQLFieldDefinition.newFieldDefinition().type(type_).name(this)
 }
 operator fun String.rangeTo(type_: GraphQLOutputType) = this.ofType(type_)
+
+fun GraphQLType.nonNull() = GraphQLNonNull(this)
+operator fun GraphQLType.not() = GraphQLNonNull(this)
+
 infix fun GraphQLFieldDefinition.Builder.staticValue(value:Any) = this.staticValue(value)
 infix fun GraphQLFieldDefinition.Builder.description(d:String) = this.description(d)
+infix fun GraphQLFieldDefinition.Builder.dataFetcher(fetcher:DataFetcher) = this.dataFetcher(fetcher)
+
+infix fun GraphQLFieldDefinition.Builder.argument(builder: GraphQLArgument.Builder) = this.argument(builder)
+
 fun newGraphQLSchema( query: GraphQLObjectType.Builder)= GraphQLSchema.newSchema().query(query)
+
+fun listOfRefs(typeName:String)=GraphQLList(GraphQLTypeReference(typeName))
+fun listOfObjs(wrappedType: GraphQLType) = GraphQLList(wrappedType)
